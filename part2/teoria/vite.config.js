a68a257f2267d434705493abfc08d339af9cd8d1
sebastian <sebastian.mojica28@gmail.com>
2025-08-13 10:49:47 -0500

    ajustando proxy en vite.config

--- part2/teoria/vite.config.js
+++ part2/teoria/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    //Se crea un proxy que redirige todas las solicitudes realizadas por el front end a la fase de desarrollo que se ejecuta en el puerto 3001
+    //De esta forma la aplicacion funciona tanto en desarrollo como en produccion
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+});
